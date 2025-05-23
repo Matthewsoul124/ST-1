4dc93690e7d20edf7fa8bd8ad6e9c8e6215b090d
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-05-23 20:19:59 +0300

    fix 5

---
+++ alg.h
@@ -0,0 +1,12 @@
+// Copyright 2025 UNN-CS
+#ifndef SRC_ALG_H_
+#define SRC_ALG_H_
+
+#include <cstdint>
+
+bool checkPrime(uint64_t value);
+uint64_t nPrime(uint64_t n);
+uint64_t nextPrime(uint64_t value);
+uint64_t sumPrime(uint64_t hbound);
+
+#endif  // SRC_ALG_H_ 

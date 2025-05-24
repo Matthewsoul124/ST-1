ee301bb4b31a8d61b4dda1328380bf8810d49298
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-05-24 08:22:11 +0300

    fix e

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,61 +1,59 @@
 // Copyright 2025 UNN-CS
 
-#include "src/alg.h"
+#include "alg.h"
 
 bool checkPrime(uint64_t value) {
-    if (value <= 1) {
-        return false;
-    }
+  if (value <= 1) {
+    return false;
+  }
 
-    for (uint64_t i = 2; i * i <= value; ++i) {
-        if (value % i == 0) {
-            return false;
-        }
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) {
-        return 0;
+  if (n == 0) {
+    return 0;
+  }
+
+  uint64_t count = 0;
+  uint64_t num = 2;
+
+  while (count < n) {
+    if (checkPrime(num)) {
+      count++;
     }
-
-    uint64_t count = 0;
-    uint64_t num = 2;
-
-    while (count < n) {
-        if (checkPrime(num)) {
-            count++;
-        }
-        if (count < n) {
-            num++;
-        }
+    if (count < n) {
+      num++;
     }
+  }
 
-    return num;
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t num = value + 1;
+  uint64_t num = value + 1;
 
-    while (true) {
-        if (checkPrime(num)) {
-            return num;
-        }
-        num++;
+  while (true) {
+    if (checkPrime(num)) {
+      return num;
     }
+    num++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
+  uint64_t sum = 0;
 
-    for (uint64_t i = 2; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i)) {
+      sum += i;
     }
+  }
 
-    return sum;
+  return sum;
 }
-
-

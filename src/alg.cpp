8ce9a47e69c84ef10ce7fb5e22f82adaf90aedbb
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-05-24 08:32:34 +0300

    fix w

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,10 +25,10 @@ uint64_t nPrime(uint64_t n) {
 
   while (count < n) {
     if (checkPrime(num)) {
-      count++;
+      ++count;
     }
     if (count < n) {
-      num++;
+      ++num;
     }
   }
 
@@ -36,19 +36,23 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t num = value + 1;
-
-  while (true) {
-    if (checkPrime(num)) {
-      return num;
-    }
-    num++;
+  if (value < 2) {
+    return 2;
   }
+
+  uint64_t num = value + 1;
+  while (!checkPrime(num)) {
+    ++num;
+  }
+  return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
+  if (hbound <= 2) {
+    return 0;
+  }
 
+  uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; ++i) {
     if (checkPrime(i)) {
       sum += i;
